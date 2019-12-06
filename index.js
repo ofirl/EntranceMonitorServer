8c644b0c7bb8f006a0efe8958585732330df13d3
Ofir Levi <ofirl24@gmail.com>
2019-12-06 18:20:47 +0200

    test

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ express()
   .post('/allGuests', express.json(), middlewares.asyncMiddleware(API.guest.allGuests))
   .post('/allExpectedGuests', express.json(), middlewares.asyncMiddleware(API.guest.allExpected))
   // .get('/addGuest', (req, res) => { res.send('yay!'); })
-  .get('/client/*', (req, res) => { res.render('client/index'); })
+  .get('/client/*', (req, res) => { res.sendFile(path.join(__dirname, 'client/index'); })
 
   // start the server
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
