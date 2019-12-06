338ada6e13a9493bd20a3a219fc78bc046cf930b
Ofir Levi <ofirl24@gmail.com>
2019-12-06 17:50:50 +0200

    test

---
+++ client/precache-manifest.1f6a62df6c53caf90c7f1239705980e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5b454d3cbf7508628e73341546e767",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89b0a98f3867a2ba7354",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "b03db4a9a5fdd7821065",
+    "url": "/static/css/main.8817f024.chunk.css"
+  },
+  {
+    "revision": "89b0a98f3867a2ba7354",
+    "url": "/static/js/2.2bc497fb.chunk.js"
+  },
+  {
+    "revision": "b03db4a9a5fdd7821065",
+    "url": "/static/js/main.608143d2.chunk.js"
+  },
+  {
+    "revision": "d8148811f2c5133c531f",
+    "url": "/static/js/runtime-main.38a95b34.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- index.js
+++ index.js
@@ -43,7 +43,7 @@ express()
 
   .get('/', (req, res) => res.render('pages/addGuest'))
   .get('/startPage', (req, res) => res.render('pages/index'))
-  .get('/test', (req, res) => { res.send('yay!'); })
+  .get('/test', (req, res) => { res.send('client/index.html'); })
   .get('/db', async (req, res) => {
     try {
       // const client = await pool.connect()
@@ -102,7 +102,8 @@ express()
   .post('/removeGuest', express.json(), middlewares.asyncMiddleware(API.guest.removeGuest))
   .post('/allGuests', express.json(), middlewares.asyncMiddleware(API.guest.allGuests))
   .post('/allExpectedGuests', express.json(), middlewares.asyncMiddleware(API.guest.allExpected))
-  .get('/addGuest', (req, res) => { res.send('yay!'); })
+  // .get('/addGuest', (req, res) => { res.send('yay!'); })
+  .get('/client/*', (req, res) => { res.send('yay!'); })
 
   // start the server
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
