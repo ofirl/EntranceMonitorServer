cec129248403e6ef61a8ce0cf9551d68779538f5
Ofir Levi <ofirl24@gmail.com>
2019-12-14 21:27:18 +0200

    changed main page

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
-  .get('/', (req, res) => res.render('pages/addGuest'))
+  // .get('/', (req, res) => res.render('pages/addGuest'))
+  .get('/', (req, res) => res.render('client/index.ejs'))
   .get('/startPage', (req, res) => res.render('pages/index'))
   .get('/test', (req, res) => { res.send('yay!'); })
   .get('/db', async (req, res) => {
