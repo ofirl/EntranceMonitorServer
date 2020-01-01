815a0636da0dbb93a648b25943a6fa02111de23f
Ofir Levi <ofirl24@gmail.com>
2020-01-01 11:17:52 +0200

    new client

--- views/client/precache-manifest.2d642d3d7e53458594519cac876a6758.js
+++ views/client/precache-manifest.2d642d3d7e53458594519cac876a6758.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa8df9e4a13328a9fe2c9c97513d371f",
+    "revision": "fb42389ce284bee89a9ae3d06b24b193",
     "url": "/index.html"
   },
   {
-    "revision": "6e54879e6ecfc9fb0c50",
+    "revision": "b28f84eeb8613723b94d",
     "url": "/static/css/2.11f871ab.chunk.css"
   },
   {
-    "revision": "398ba468d904cb290bc3",
+    "revision": "855075421417d2630e7b",
     "url": "/static/css/main.8817f024.chunk.css"
   },
   {
-    "revision": "6e54879e6ecfc9fb0c50",
-    "url": "/static/js/2.b3151f43.chunk.js"
+    "revision": "b28f84eeb8613723b94d",
+    "url": "/static/js/2.e4fd94ef.chunk.js"
   },
   {
     "revision": "d30b9d4805159bb483288ae8cff70380",
-    "url": "/static/js/2.b3151f43.chunk.js.LICENSE"
+    "url": "/static/js/2.e4fd94ef.chunk.js.LICENSE"
   },
   {
-    "revision": "398ba468d904cb290bc3",
-    "url": "/static/js/main.ec6c60c1.chunk.js"
+    "revision": "855075421417d2630e7b",
+    "url": "/static/js/main.0ab58134.chunk.js"
   },
   {
     "revision": "efa8e4346febb777ce94",
--- views/client/service-worker.js
+++ views/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25477fdb51c0abcbb23f32aff130e5bb.js"
+  "/precache-manifest.2d642d3d7e53458594519cac876a6758.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
